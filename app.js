434a334403587c8e89331e861b642a2851d44cc2
anaes0 <anaesvg@gmail.com>
2023-02-05 17:16:01 +0000

    updates

--- app.js
+++ app.js
@@ -1,25 +1,62 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 });
 
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
+
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+  var opciones = document.querySelectorAll("#links  a");
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  opciones[4].className = "";
+  link.className = "seleccionado";
+
+  //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+  //en modo responsive
+  var x = document.getElementById("nav");
+  x.className = "";
+}
+
+//funcion que aplica la animación de la barra de habilidades
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+
+  if (distancia_skills >= 300) {
+    document.getElementById("html").classList.add("barra-progreso1");
+    document.getElementById("js").classList.add("barra-progreso2");
+    document.getElementById("re").classList.add("barra-progreso3");
+    document.getElementById("git").classList.add("barra-progreso4");
+    document.getElementById("sql").classList.add("barra-progreso5");
+    document.getElementById("php").classList.add("barra-progreso6");
+  }
+}
